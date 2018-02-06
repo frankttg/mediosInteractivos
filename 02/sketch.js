c28894354b1f933faff7ab39e4bc168123c3fbe6
frankttg <35973390+frankttg@users.noreply.github.com>
2018-02-05 21:52:15 -0500

    Add files via upload

---
+++ 02/sketch.js
@@ -0,0 +1,848 @@
+var posX = 0;
+var posY = 0;
+
+function setup() {
+  createCanvas(330, 560);
+  background(209, 196, 166);
+
+  for (var posX = 0; posX < 30; posX = posX + 15) { // 4 rectángulos negros
+    noStroke();
+    fill(0);
+    rect(posX, posY, 15, 15);
+    posX = posX + 2;
+  }
+
+  posY = posY + 15;
+  posX = 0;
+
+  for (var posX = 0; posX < 30; posX = posX + 15) {
+    noStroke();
+    fill(0);
+    rect(posX, posY, 15, 15);
+    posX = posX + 2;
+  }
+
+  posY = posY + 15;
+  posX = 0;
+
+
+  for (var posX = 0; posX < 30; posX = posX + 15) {
+    noStroke();
+    fill(0);
+    rect(posX, posY, 15, 15);
+    posX = posX + 2;
+  }
+
+  posY = posY + 17;
+  posX = 0;
+
+  for (var posX = 0; posX < 30; posX = posX + 15) {
+    noStroke();
+    fill(0);
+    rect(posX, posY, 15, 15);
+    posX = posX + 2;
+  }
+
+  posY = posY + 15;
+  posX = 0;
+
+  for (var posX = 0; posX < 30; posX = posX + 15) {
+    noStroke();
+    fill(0);
+    rect(posX, posY, 15, 15);
+    posX = posX + 2;
+  }
+
+  posY = posY + 15;
+  posX = 0;
+
+  for (var posX = 0; posX < 30; posX = posX + 15) { // End
+    noStroke();
+    fill(0);
+    rect(posX, posY, 15, 15);
+    posX = posX + 2;
+  }
+
+  posY = 0;
+
+
+  for (var posX = 32; posX < 55; posX = posX + 6.5) { // 6 líneas rojas
+    noStroke();
+    fill(188, 68, 50);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+
+  posY = 15;
+
+  for (var posX = 32; posX < 55; posX = posX + 6.5) {
+    noStroke();
+    fill(188, 68, 50);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 30;
+
+
+  for (var posX = 32; posX < 55; posX = posX + 6.5) {
+    noStroke();
+    fill(188, 68, 50);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 45;
+
+  for (var posX = 32; posX < 55; posX = posX + 6.5) {
+    noStroke();
+    fill(188, 68, 50);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 60;
+
+  for (var posX = 32; posX < 55; posX = posX + 6.5) {
+    noStroke();
+    fill(188, 68, 50);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 75;
+
+  for (var posX = 32; posX < 55; posX = posX + 6.5) {
+    noStroke();
+    fill(188, 68, 50);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 0
+
+  for (var posX = 55; posX < 78; posX = posX + 6.5) { // 6 líneas cafés
+    noStroke();
+    fill(74, 61, 40);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 7.5;
+
+  for (var posX = 55; posX < 78; posX = posX + 6.5) { // relleno oscuro
+    noStroke();
+    fill(179, 169, 134);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+
+  posY = 15;
+
+  for (var posX = 55; posX < 78; posX = posX + 6.5) {
+    noStroke();
+    fill(74, 61, 40);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 22.5;
+
+  for (var posX = 55; posX < 78; posX = posX + 6.5) {
+    noStroke();
+    fill(179, 169, 134);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 30;
+
+  for (var posX = 55; posX < 78; posX = posX + 6.5) {
+    noStroke();
+    fill(74, 61, 40);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 37.5;
+
+
+  for (var posX = 55; posX < 78; posX = posX + 6.5) {
+    noStroke();
+    fill(179, 169, 134);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 45;
+
+  for (var posX = 55; posX < 78; posX = posX + 6.5) {
+    noStroke();
+    fill(74, 61, 40);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 52.5;
+
+
+  for (var posX = 55; posX < 78; posX = posX + 6.5) {
+    noStroke();
+    fill(179, 169, 134);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 60;
+
+  for (var posX = 55; posX < 78; posX = posX + 6.5) {
+    noStroke();
+    fill(74, 61, 40);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 67.5;
+
+
+  for (var posX = 55; posX < 78; posX = posX + 6.5) {
+    noStroke();
+    fill(179, 169, 134);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 75;
+
+  for (var posX = 55; posX < 78; posX = posX + 6.5) {
+    noStroke();
+    fill(74, 61, 40);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 82.5;
+
+  for (var posX = 55; posX < 78; posX = posX + 6.5) {
+    noStroke();
+    fill(179, 169, 134);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 0;
+
+
+  for (var posX = 80; posX < 108; posX = posX + 15) { // 4 rectángulos negros
+    noStroke();
+    fill(0);
+    rect(posX, posY, 15, 15);
+    posX = posX + 2;
+  }
+
+  posY = posY + 15;
+  posX = 0;
+
+  for (var posX = 80; posX < 108; posX = posX + 15) {
+    noStroke();
+    fill(0);
+    rect(posX, posY, 15, 15);
+    posX = posX + 2;
+  }
+
+  posY = posY + 15;
+  posX = 0;
+
+
+  for (var posX = 80; posX < 108; posX = posX + 15) {
+    noStroke();
+    fill(0);
+    rect(posX, posY, 15, 15);
+    posX = posX + 2;
+  }
+
+  posY = posY + 17;
+  posX = 0;
+
+  for (var posX = 80; posX < 108; posX = posX + 15) {
+    noStroke();
+    fill(0);
+    rect(posX, posY, 15, 15);
+    posX = posX + 2;
+  }
+
+  posY = posY + 15;
+  posX = 0;
+
+  for (var posX = 80; posX < 108; posX = posX + 15) {
+    noStroke();
+    fill(0);
+    rect(posX, posY, 15, 15);
+    posX = posX + 2;
+  }
+
+  posY = posY + 15;
+  posX = 0;
+
+  for (var posX = 80; posX < 108; posX = posX + 15) { // End
+    noStroke();
+    fill(0);
+    rect(posX, posY, 15, 15);
+    posX = posX + 2;
+  }
+
+  posY = 0;
+
+  for (var posX = 112; posX < 138; posX = posX + 6.5) { // 6 líneas cafés
+    noStroke();
+    fill(74, 61, 40);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 7.5;
+
+  for (var posX = 112; posX < 138; posX = posX + 6.5) { // relleno oscuro
+    noStroke();
+    fill(179, 169, 134);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+
+  posY = 15;
+
+  for (var posX = 112; posX < 138; posX = posX + 6.5) {
+    noStroke();
+    fill(74, 61, 40);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 22.5;
+
+  for (var posX = 112; posX < 138; posX = posX + 6.5) {
+    noStroke();
+    fill(179, 169, 134);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 30;
+
+  for (var posX = 112; posX < 138; posX = posX + 6.5) {
+    noStroke();
+    fill(74, 61, 40);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 37.5;
+
+
+  for (var posX = 112; posX < 138; posX = posX + 6.5) {
+    noStroke();
+    fill(179, 169, 134);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 45;
+
+  for (var posX = 112; posX < 138; posX = posX + 6.5) {
+    noStroke();
+    fill(74, 61, 40);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 52.5;
+
+
+  for (var posX = 112; posX < 138; posX = posX + 6.5) {
+    noStroke();
+    fill(179, 169, 134);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 60;
+
+  for (var posX = 112; posX < 138; posX = posX + 6.5) {
+    noStroke();
+    fill(74, 61, 40);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 67.5;
+
+
+  for (var posX = 112; posX < 138; posX = posX + 6.5) {
+    noStroke();
+    fill(179, 169, 134);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 75;
+
+  for (var posX = 112; posX < 138; posX = posX + 6.5) {
+    noStroke();
+    fill(74, 61, 40);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 82.5;
+
+  for (var posX = 112; posX < 138; posX = posX + 6.5) {
+    noStroke();
+    fill(179, 169, 134);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 0;
+
+  for (var posX = 138; posX < 168; posX = posX + 15) { // 4 rectángulos negros
+    noStroke();
+    fill(0);
+    rect(posX, posY, 15, 15);
+    posX = posX + 2;
+  }
+
+  posY = posY + 15;
+  posX = 0;
+
+  for (var posX = 138; posX < 168; posX = posX + 15) {
+    noStroke();
+    fill(0);
+    rect(posX, posY, 15, 15);
+    posX = posX + 2;
+  }
+
+  posY = posY + 15;
+  posX = 0;
+
+
+  for (var posX = 138; posX < 168; posX = posX + 15) {
+    noStroke();
+    fill(0);
+    rect(posX, posY, 15, 15);
+    posX = posX + 2;
+  }
+
+  posY = posY + 17;
+  posX = 0;
+
+  for (var posX = 138; posX < 168; posX = posX + 15) {
+    noStroke();
+    fill(0);
+    rect(posX, posY, 15, 15);
+    posX = posX + 2;
+  }
+
+  posY = posY + 15;
+  posX = 0;
+
+  for (var posX = 138; posX < 168; posX = posX + 15) {
+    noStroke();
+    fill(0);
+    rect(posX, posY, 15, 15);
+    posX = posX + 2;
+  }
+
+  posY = posY + 15;
+  posX = 0;
+
+  for (var posX = 138; posX < 168; posX = posX + 15) { // End
+    noStroke();
+    fill(0);
+    rect(posX, posY, 15, 15);
+    posX = posX + 2;
+  }
+
+  posY = 0;
+
+  for (var posX = 170; posX < 190; posX = posX + 6.5) { // 6 líneas rojas
+    noStroke();
+    fill(188, 68, 50);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+
+  posY = 15;
+
+  for (var posX = 170; posX < 190; posX = posX + 6.5) {
+    noStroke();
+    fill(188, 68, 50);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 30;
+
+
+  for (var posX = 170; posX < 190; posX = posX + 6.5) {
+    noStroke();
+    fill(188, 68, 50);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 45;
+
+  for (var posX = 170; posX < 190; posX = posX + 6.5) {
+    noStroke();
+    fill(188, 68, 50);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 60;
+
+  for (var posX = 170; posX < 190; posX = posX + 6.5) {
+    noStroke();
+    fill(188, 68, 50);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 75;
+
+  for (var posX = 170; posX < 190; posX = posX + 6.5) {
+    noStroke();
+    fill(188, 68, 50);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 0;
+
+  for (var posX = 192; posX < 218; posX = posX + 6.5) { // 6 líneas cafés
+    noStroke();
+    fill(74, 61, 40);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 7.5;
+
+  for (var posX = 192; posX < 218; posX = posX + 6.5) { // relleno oscuro
+    noStroke();
+    fill(179, 169, 134);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+
+  posY = 15;
+
+  for (var posX = 192; posX < 218; posX = posX + 6.5) {
+    noStroke();
+    fill(74, 61, 40);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 22.5;
+
+  for (var posX = 192; posX < 218; posX = posX + 6.5) {
+    noStroke();
+    fill(179, 169, 134);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 30;
+
+  for (var posX = 192; posX < 218; posX = posX + 6.5) {
+    noStroke();
+    fill(74, 61, 40);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 37.5;
+
+
+  for (var posX = 192; posX < 218; posX = posX + 6.5) {
+    noStroke();
+    fill(179, 169, 134);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 45;
+
+  for (var posX = 192; posX < 218; posX = posX + 6.5) {
+    noStroke();
+    fill(74, 61, 40);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 52.5;
+
+
+  for (var posX = 192; posX < 218; posX = posX + 6.5) {
+    noStroke();
+    fill(179, 169, 134);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 60;
+
+  for (var posX = 192; posX < 218; posX = posX + 6.5) {
+    noStroke();
+    fill(74, 61, 40);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 67.5;
+
+
+  for (var posX = 192; posX < 218; posX = posX + 6.5) {
+    noStroke();
+    fill(179, 169, 134);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 75;
+
+  for (var posX = 192; posX < 218; posX = posX + 6.5) {
+    noStroke();
+    fill(74, 61, 40);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 82.5;
+
+  for (var posX = 192; posX < 218; posX = posX + 6.5) {
+    noStroke();
+    fill(179, 169, 134);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 0;
+
+  for (var posX = 219; posX < 240; posX = posX + 6.5) { // 6 líneas rojas
+    noStroke();
+    fill(188, 68, 50);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+
+  posY = 15;
+
+  for (var posX = 219; posX < 240; posX = posX + 6.5) {
+    noStroke();
+    fill(188, 68, 50);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 30;
+
+
+  for (var posX = 219; posX < 240; posX = posX + 6.5) {
+    noStroke();
+    fill(188, 68, 50);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 45;
+
+  for (var posX = 219; posX < 240; posX = posX + 6.5) {
+    noStroke();
+    fill(188, 68, 50);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 60;
+
+  for (var posX = 219; posX < 240; posX = posX + 6.5) {
+    noStroke();
+    fill(188, 68, 50);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 75;
+
+  for (var posX = 219; posX < 240; posX = posX + 6.5) {
+    noStroke();
+    fill(188, 68, 50);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 0;
+
+  for (var posX = 245; posX < 272; posX = posX + 15) { // 4 rectángulos negros
+    noStroke();
+    fill(0);
+    rect(posX, posY, 15, 15);
+    posX = posX + 2;
+  }
+
+  posY = posY + 15;
+  posX = 0;
+
+  for (var posX = 245; posX < 272; posX = posX + 15) {
+    noStroke();
+    fill(0);
+    rect(posX, posY, 15, 15);
+    posX = posX + 2;
+  }
+
+  posY = posY + 15;
+  posX = 0;
+
+
+  for (var posX = 245; posX < 272; posX = posX + 15) {
+    noStroke();
+    fill(0);
+    rect(posX, posY, 15, 15);
+    posX = posX + 2;
+  }
+
+  posY = posY + 17;
+  posX = 0;
+
+  for (var posX = 245; posX < 272; posX = posX + 15) {
+    noStroke();
+    fill(0);
+    rect(posX, posY, 15, 15);
+    posX = posX + 2;
+  }
+
+  posY = posY + 15;
+  posX = 0;
+
+  for (var posX = 245; posX < 272; posX = posX + 15) {
+    noStroke();
+    fill(0);
+    rect(posX, posY, 15, 15);
+    posX = posX + 2;
+  }
+
+  posY = posY + 15;
+  posX = 0;
+
+  for (var posX = 245; posX < 272; posX = posX + 15) { // End
+    noStroke();
+    fill(0);
+    rect(posX, posY, 15, 15);
+    posX = posX + 2;
+  }
+
+  posY = 0;
+
+  for (var posX = 277; posX < 297; posX = posX + 6.5) { // 6 líneas cafés
+    noStroke();
+    fill(74, 61, 40);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 7.5;
+
+  for (var posX = 277; posX < 297; posX = posX + 6.5) { // relleno oscuro
+    noStroke();
+    fill(179, 169, 134);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+
+  posY = 15;
+
+  for (var posX = 277; posX < 297; posX = posX + 6.5) {
+    noStroke();
+    fill(74, 61, 40);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 22.5;
+
+  for (var posX = 277; posX < 297; posX = posX + 6.5) {
+    noStroke();
+    fill(179, 169, 134);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 30;
+
+  for (var posX = 277; posX < 297; posX = posX + 6.5) {
+    noStroke();
+    fill(74, 61, 40);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 37.5;
+
+
+  for (var posX = 277; posX < 297; posX = posX + 6.5) {
+    noStroke();
+    fill(179, 169, 134);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 45;
+
+  for (var posX = 277; posX < 297; posX = posX + 6.5) {
+    noStroke();
+    fill(74, 61, 40);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 52.5;
+
+
+  for (var posX = 277; posX < 297; posX = posX + 6.5) {
+    noStroke();
+    fill(179, 169, 134);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 60;
+
+  for (var posX = 277; posX < 297; posX = posX + 6.5) {
+    noStroke();
+    fill(74, 61, 40);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 67.5;
+
+
+  for (var posX = 277; posX < 297; posX = posX + 6.5) {
+    noStroke();
+    fill(179, 169, 134);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 75;
+
+  for (var posX = 277; posX < 297; posX = posX + 6.5) {
+    noStroke();
+    fill(74, 61, 40);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 82.5;
+
+  for (var posX = 277; posX < 297; posX = posX + 6.5) {
+    noStroke();
+    fill(179, 169, 134);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 0;
+
+  for (var posX = 304; posX < 330; posX = posX + 6.5) { // 6 líneas rojas
+    noStroke();
+    fill(188, 68, 50);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+
+  posY = 15;
+
+  for (var posX = 304; posX < 330; posX = posX + 6.5) {
+    noStroke();
+    fill(188, 68, 50);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 30;
+
+
+  for (var posX = 304; posX < 330; posX = posX + 6.5) {
+    noStroke();
+    fill(188, 68, 50);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 45;
+
+  for (var posX = 304; posX < 330; posX = posX + 6.5) {
+    noStroke();
+    fill(188, 68, 50);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 60;
+
+  for (var posX = 304; posX < 330; posX = posX + 6.5) {
+    noStroke();
+    fill(188, 68, 50);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+  posY = 75;
+
+  for (var posX = 304; posX < 330; posX = posX + 6.5) {
+    noStroke();
+    fill(188, 68, 50);
+    rect(posX, posY, 7.5, 7.5);
+  }
+
+}
